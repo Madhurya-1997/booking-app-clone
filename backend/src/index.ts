69aeff892e885c7d98f9b6d01c587917a7e082bf
Madhurya Bharadwaaz <dg.hipl@gmail.com>
2024-01-22 20:23:19 +0530

    temp commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,12 +22,13 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-    cors({
-        origin: process.env.FRONTEND_BASE_URL,
-        credentials: true,
-    })
-);
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: process.env.FRONTEND_BASE_URL,
+//         credentials: true,
+//     })
+// );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
